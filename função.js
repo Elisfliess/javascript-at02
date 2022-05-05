bc0d8035dc4e8ce6f7f019b1c7f4dc69cc328e7e
ElisFliess <102757856+Elisfliess@users.noreply.github.com>
2022-05-05 15:41:12 -0300

    Add files via upload

---
+++ função.js
@@ -0,0 +1,36 @@
+//1
+
+function nome (b){
+    console.log(b)
+    return
+}
+
+nome('lise');
+
+//2
+function soma (b,c){
+    let res =  x+y 
+    console.log(res)
+    return
+}
+
+soma(4,8)
+
+
+//3
+function soma (b,c){
+    console.log(b*c)
+}
+
+soma (5,9)
+
+//4
+
+function conta (r,s,t,u){
+    let res = (r+s+t+u)/4;
+    console.log(res)
+    return
+}
+
+conta(5,6,7,8)
+
